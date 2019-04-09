a01ba1273b52c2a77376c800e59e4d758ab60bbc
Bintang Yuwono <bintangby@gmail.com>
2019-04-09 16:22:08 +0200

    fix utils.py so that stickler success

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-print('this is to test stickler to find the file does not successfully pass the test for line length limits')
+print('this is to test stickler to find the file does successfully')
 
 def funny(x, y):
     return x - 2*y
