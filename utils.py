da87e97322b34a6584c77fe0c328a1cd280b2951
Bintang Yuwono <bintangby@gmail.com>
2019-04-09 15:54:24 +0200

    update on utils so that unit tests will show fail

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+print('this is to test stickler to find the file does not successfully pass the test for line length limits')
 
 def funny(x, y):
-    return x - y
+    return x - 2*y
